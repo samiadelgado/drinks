80ec03424d4508075a4ed3d0bf4e7675ea06b193
Sami A.Delgado <samiadelgado@gmail.com>
2020-09-23 19:53:15 +0200

    Drinks cocktail

---
+++ precache-manifest.ee9c50f498176af285e641e668a89e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de922fa3370a693b41391231c0291879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d144d2c10e5b5b64880",
+    "url": "/static/css/main.011ae914.chunk.css"
+  },
+  {
+    "revision": "b5ceafcde226f363e377",
+    "url": "/static/js/2.bfbb91fd.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/static/js/2.bfbb91fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d144d2c10e5b5b64880",
+    "url": "/static/js/main.d4d43610.chunk.js"
+  },
+  {
+    "revision": "b4aaad1baad548ac4779",
+    "url": "/static/js/runtime-main.34dd3913.js"
+  }
+]);
